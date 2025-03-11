64e54dd78bdf1d0673ed6bc4ed58803fda5ee2f8
gulzanutekina <gulzhan.utekina@gmail.com>
2025-03-11 14:19:41 +0500

    dgd

--- codeGulzhan.py
+++ codeGulzhan.py
@@ -1,38 +1,17 @@
+#part 1
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
-df = pd.read_csv("air_quality_data.csv")
+#1 read air quality data
+file_path="air_quality_data.csv"
+df=pd.read_csv(file_path)
+#2 store CO levels for sensor ID A103 as lists
+#3Allow user input to retrieve sensor readings for a given sensor
+sensor_id=input("Enter Sensor ID:")
 
-time_col = 'Timestamp'
-if 'Timestamp' in df.columns:
-    df['Timestamp'] = pd.to_datetime(df['Timestamp'])
-elif 'Time' in df.columns:
-    df['Time'] = pd.to_datetime(df['Time'])
-    time_col = 'Time'
-else:
-    print("No timestamp column found!")
-
-plt.figure(figsize=(15, 10))
-
-plt.subplot(2,2,1)
-sns.lineplot(x=df[time_col], y=df['PM2.5 (microg/m3)'])
-plt.title("PM2.5 vs Timestamps")
-plt.xticks(rotation=45)
-
-plt.subplot(2,2,2)
-sns.scatterplot(x=df['PM2.5 (microg/m3)'], y=df['PM10 (microg/m3)'])
-plt.title("PM2.5 vs PM10")
-
-plt.subplot(2,2,3)
-corr_matrix = df[['PM2.5 (microg/m3)', 'PM10 (microg/m3)', 'CO (ppm)']].corr()
-sns.heatmap(corr_matrix, cmap='coolwarm', annot=True)
-plt.title("Correlation Heatmap")
-
-plt.subplot(2,2,4)
-sns.kdeplot(x=df['PM2.5 (microg/m3)'].dropna(), fill=True)
-plt.title("PDF of PM2.5")
+#4Identify and print the mean value for CO level readings
 
 
-plt.tight_layout()
-plt.show()
+
